b1ec090548d2edf05f5c2333f3579f6edd29b9b8
Klove Adams <kloveadams@gmail.com>
2021-10-11 16:37:00 -0400

    user-router wierd get wroks

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
-const router = require("express").Router;
+const router = require("express").Router();
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-const { restricted } = require("../auth/auth-middleware");
+// const { restricted } = require("../auth/auth-middleware");
 const Users = require("./users-model");
 
 /**
@@ -26,13 +26,14 @@ const Users = require("./users-model");
   }
  */
 
-  router.get("/", (req, res, next) => {
-    Users.find()
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch(next);
+router.get("/", (req, res, next) => {
+  Users.find()
+  .then((users) => {
+    res.status(200).json(users);
   })
+  // res.json("inside users")
+  .catch(next);
+});
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router;
